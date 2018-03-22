0d9b62766c9c2ce8549d874011f9f2f23b05ee28
Steven Nemetz <snemetz@wiser.com>
2018-03-22 11:19:11 -0700

    Add output

--- outputs.tf
+++ outputs.tf
@@ -1,1 +1,8 @@
+output "namespace_id" {
+  description = "ID of service discovery namespace"
 
+  value = "${element(concat(
+    aws_service_discovery_private_dns_namespace.this.*.id,
+    aws_service_discovery_public_dns_namespace.this.*.id,
+    list("")), 0)}"
+}
